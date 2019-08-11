c1114f518b12375f192f50c4027a271f49bb4bad
shmousavi <shm@shm.com>
2019-08-11 13:42:18 +0000

    update the launch files

--- scripts/pick_objects.sh
+++ scripts/pick_objects.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
-xterm  -e  "source ../../devel/setup.bash; roslaunch turtlebot_gazebo turtlebot_world.launch" &
+xterm  -e  "source ../../../devel/setup.bash; roslaunch turtlebot_gazebo turtlebot_world.launch" &
 
 sleep 5
 
-xterm  -e  "source ../../devel/setup.bash; roslaunch turtlebot_gazebo amcl_demo.launch" & 
+xterm  -e  "source ../../../devel/setup.bash; roslaunch turtlebot_gazebo amcl_demo.launch" & 
 
 sleep 5
 
-xterm  -e  "source ../../devel/setup.bash; roslaunch turtlebot_rviz_launchers view_navigation.launch" &
+xterm  -e  "source ../../../devel/setup.bash; roslaunch turtlebot_rviz_launchers view_navigation.launch" &
 
 sleep 5
 
-xterm  -e  "source ../../devel/setup.bash; rosrun pick_objects pick_objects"
+xterm  -e  "source ../../../devel/setup.bash; rosrun pick_objects pick_objects"
--- scripts/test_slam.sh
+++ scripts/test_slam.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
-xterm  -e  "source ../../devel/setup.bash; roslaunch turtlebot_gazebo turtlebot_world.launch" &
+xterm  -e  "source ../../../devel/setup.bash; roslaunch turtlebot_gazebo turtlebot_world.launch" &
 
 sleep 5
 
-xterm  -e  "source ../../devel/setup.bash; roslaunch turtlebot_gazebo gmapping_demo.launch" & 
+xterm  -e  "source ../../../devel/setup.bash; roslaunch turtlebot_gazebo gmapping_demo.launch" & 
 
 sleep 5
 
-xterm  -e  "source ../../devel/setup.bash; roslaunch turtlebot_rviz_launchers view_navigation.launch" & 
+xterm  -e  "source ../../../devel/setup.bash; roslaunch turtlebot_rviz_launchers view_navigation.launch" & 
 
 sleep 5
 
-xterm  -e  "source ../../devel/setup.bash; roslaunch turtlebot_teleop keyboard_teleop.launch"
+xterm  -e  "source ../../../devel/setup.bash; roslaunch turtlebot_teleop keyboard_teleop.launch"
